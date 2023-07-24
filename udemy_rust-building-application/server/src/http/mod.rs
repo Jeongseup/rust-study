a9c6883bcb488f8bbbaa507da046d0bc675a65b0
Jeongseup <seup87@gmail.com>
2023-07-24 23:19:59 +0900

    study

--- udemy_rust-building-application/server/src/http/mod.rs
+++ udemy_rust-building-application/server/src/http/mod.rs
@@ -2,6 +2,11 @@ pub use method::Method; // for exposing http::Method at main.rs
 pub use query_string::{QueryString, Value as QueryStringValue};
 pub use request::ParseError;
 pub use request::Request; // for use http::Request at main.rs
+pub use response::Response;
+pub use status_code::StatusCode;
+
 pub mod method;
 pub mod query_string;
 pub mod request;
+pub mod response;
+pub mod status_code;
--- udemy_rust-building-application/server/src/server.rs
+++ udemy_rust-building-application/server/src/server.rs
@@ -1,8 +1,7 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
-
 pub struct Server {
     addr: String,
 }
@@ -29,11 +28,23 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(
+                                        StatusCode::Ok,
+                                        Some("<h1>Hello </h1>".to_string()),
+                                    )
+                                    // response.send(&mut stream);
                                 }
-                                Err(e) => println!("Faield to parse err: {}", e),
+                                Err(e) => {
+                                    println!("Faield to parse err: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Faield to send response {}", e);
                             }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
